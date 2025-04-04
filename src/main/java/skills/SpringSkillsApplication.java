eb706e114c4c3b3a7bbb3c212e31c3d29e4a9fa0
Guilherme Bigois <guilhermebigois@outlook.com>
2025-04-03 21:49:30 -0300

    feat: start project

---
+++ src/main/java/skills/SpringSkillsApplication.java
@@ -0,0 +1,13 @@
+package skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skills/SpringSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
